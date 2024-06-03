104b6a2c463fe4f9f0ab0992821fe3400e4f58ee
isantiago1 <isantiagom@gmail.com>
2024-06-03 18:03:47 +0200

    Fin Punto 4, rama_2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -39,8 +39,6 @@ public class App {
         	    			.build()  		     			
         };
         		 
-  		 
-        
     	System.out.println("Recorriendo el array Empleado  -------------------");
     	for(Persona emp : arrayEmpleados)
     		System.out.println(emp);
@@ -57,9 +55,84 @@ public class App {
 			sumatoriaDeLosSalarios +=  emp.getSalario();
 		
     	};
-		
-		salarioPromedio = sumatoriaDeLosSalarios / totalDePersonas;
+    	salarioPromedio = sumatoriaDeLosSalarios / totalDePersonas;
 		System.out.println("El salario promedio de todas las personas es " + salarioPromedio);
-		System.out.println("--------------------------------------------------");     
+		System.out.println("--------------------------------------------------"); 
+    	
+		// variables;
+		
+		double sumatoriaContabilidad = 0.0;
+		double sumatoriaInformatica = 0.0;
+		double sumatoriaRrhh = 0.0;
+		
+		int totalDepContabilidad = 0;
+    	int totalDepInformatica = 0;
+    	int totalDepRrhh = 0;
+    	
+    	double salarioPromedioContabilidad = 0.0;
+    	double salarioPromedioInformatica = 0.0;
+    	double salarioPromedioRrhh = 0.0;
+		
+    	for(Empleado emp1 : arrayEmpleados) {
+    		if(emp1.getDepartamento().equals(Departamento.CONTABILIDAD)) {
+    			sumatoriaContabilidad += emp1.getSalario();
+    			totalDepContabilidad++;
+    		} else if(emp1.getDepartamento().equals(Departamento.INFORMATICA)) {
+    			sumatoriaInformatica += emp1.getSalario();
+    			totalDepInformatica++;
+    		} else {
+    			sumatoriaRrhh += emp1.getSalario();
+    			totalDepRrhh++;
+    		}
+    	};
+		salarioPromedioContabilidad = sumatoriaContabilidad / totalDepContabilidad;
+		System.out.println("El salario promedio del Dep Contabilidad " + salarioPromedioContabilidad);
+		System.out.println("--------------------------------------------------");	
+		salarioPromedioInformatica = sumatoriaInformatica / totalDepInformatica;
+		System.out.println("El salario promedio del Dep Informatico " + salarioPromedioInformatica);
+		System.out.println("--------------------------------------------------");
+		salarioPromedioRrhh = sumatoriaRrhh / totalDepRrhh;
+		System.out.println("El salario promedio del Dep RRHH " + salarioPromedioRrhh);
+		System.out.println("--------------------------------------------------");
+    	
+    	// variables;
+    	double sumatoriaSalariosHombre = 0.0;
+    	double sumatoriaSalarioMujer = 0.0;
+    	double sumatoriaSalarioOtro = 0.0;
+    	
+    	int totalGeneroHombre = 0;
+    	int totalGeneroMujer = 0;
+    	int totalGeneroOtro = 0;
+    	
+    	double salarioPromedioHombres = 0.0;
+    	double salarioPromedioMujeres = 0.0;
+    	double salarioPromedioOtro = 0.0;
+    	
+    	for(Empleado emp2 : arrayEmpleados) {
+    		if(emp2.getGenero().equals(Genero.HOMBRE)) {
+    			sumatoriaSalariosHombre += emp2.getSalario();
+    			totalGeneroHombre++;
+    		} else if(emp2.getGenero().equals(Genero.MUJER)) {
+    			sumatoriaSalarioMujer += emp2.getSalario();
+    			totalGeneroMujer++;
+    		} else {
+    			sumatoriaSalarioOtro += emp2.getSalario();
+    			totalGeneroOtro++;
+    		}
+    	};
+		salarioPromedioHombres = sumatoriaSalariosHombre / totalGeneroHombre;
+		System.out.println("El salario promedio de los Hombres " + salarioPromedioHombres);
+		System.out.println("--------------------------------------------------");	
+		salarioPromedioMujeres = sumatoriaSalarioMujer / totalGeneroMujer;
+		System.out.println("El salario promedio de las Mujeres " + salarioPromedioMujeres);
+		System.out.println("--------------------------------------------------");
+		salarioPromedioOtro = sumatoriaSalarioOtro / totalGeneroOtro;
+		System.out.println("El salario promedio de los Otros " + salarioPromedioOtro);
+		System.out.println("--------------------------------------------------");
+		
+		
+		
+		
+		
     }
 }
