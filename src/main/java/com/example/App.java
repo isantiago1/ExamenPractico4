79430b60ed6ee14eb2fd26eaa557fb3352f05d49
isantiago1 <isantiagom@gmail.com>
2024-06-03 17:29:20 +0200

    rama2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,6 @@ public class App {
 	public static void main( String[] args )
     {
         System.out.println( "Examen 4!" );
-        
     
         		 Empleado[] arrayEmpleados = {
         	        		Empleado.builder()
@@ -39,10 +38,28 @@ public class App {
         	    			.salario(3000.00)
         	    			.build()  		     			
         };
+        		 
+  		 
         
     	System.out.println("Recorriendo el array Empleado  -------------------");
     	for(Persona emp : arrayEmpleados)
     		System.out.println(emp);
-        
+    	System.out.println("--------------------------------------------------");
+    	// variables
+       	double sumatoriaDeLosSalarios = 0.0;
+    	int totalDePersonas = arrayEmpleados.length;
+    	double salarioPromedio = 0.0;
+    	
+    	//for mejorado
+    	for(Empleado emp: arrayEmpleados ) {
+    		// sumatoriaDeLosSalarios =  sumatoriaDeLosSalarios + emp.getSalario();
+    		// mejorado sentencia de asignacion compuesta
+			sumatoriaDeLosSalarios +=  emp.getSalario();
+		
+    	};
+		
+		salarioPromedio = sumatoriaDeLosSalarios / totalDePersonas;
+		System.out.println("El salario promedio de todas las personas es " + salarioPromedio);
+		System.out.println("--------------------------------------------------");     
     }
 }
